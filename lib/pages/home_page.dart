1cc4b9450bc23cdd94c31f3c71aa41ce979be39a
Daniel <daniel_sousa.p@outlook.com>
2021-04-18 17:58:06 -0300

    feat: create appBar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
+      ),
+    );
+  }
+}
